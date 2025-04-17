ae76ff28bf01bb6c165bb3c06a85518a357faa9c
Sam Wu <59162532+1523090155@users.noreply.github.com>
2025-04-17 03:37:10 +0000

    w

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  // 加载环境变量
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '~@': path.resolve(__dirname, './src')
-      }
-    },
-    define: {
-      'process.env': env
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@import "@/assets/css/style.css";`
-        }
-      }
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'vue': 'vue/dist/vue.esm-bundler.js'
     }
   }
 })
