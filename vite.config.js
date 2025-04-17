931ef30b0376d39d58d4da72d7a1c6cf7069a0ec
Sam Wu <example.a@qq.com>
2025-04-17 10:39:41 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  root: './src', // or wherever your entry file is located
   plugins: [vue()],
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY)
-  }
 })
