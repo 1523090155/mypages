1e032ec9682b6a86f871b35004a9d3163b813624
Sam Wu <59162532+1523090155@users.noreply.github.com>
2025-04-17 03:59:33 +0000

    2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue']
     }
   }
 })
